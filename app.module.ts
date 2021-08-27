a40c51e258cdd4332d1d52e89429196772dd0898
Thiago Moraes <thiago4x70@gmail.com>
2021-08-27 13:58:25 -0300

    remove files

--- app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CampersController } from './campers/campers.controller';
-
-@Module({
-  imports: [],
-  controllers: [CampersController],
-  providers: [],
-})
-export class AppModule {}
